59c755eef50a7e0dfedfbfc2a3c3e89faefb1e08
Cantum2 <Cantumarcus19@gmail.com>
2017-03-22 18:26:11 -0600

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,50 @@
+/*this program will take in user input from the console and eventually be exported to a
+sql database
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+//creating account method
+int createAccount()
+{
+	//initilizing variables
+	string userName, passWord;
+
+	//username
+	cout << "Enter username: ";
+	cin >> userName; 
+
+	//password
+	cout << "Enter password: "; 
+	cin >> passWord;  
+
+	return 0;  
+
+}
+
+int main()
+{
+	//initilizing variables
+	string userName, passWord;
+	char accountAnswer;
+
+	//Prompting if user has created account
+	cout << "Do you have an account? Y or N ";
+		cin >> accountAnswer;
+
+	//if account the user has entered N for previous question console prints you must create account
+	if (accountAnswer == 'N' || accountAnswer=='n')
+	{
+		cout << "You must create an account. "; 
+
+	}
+
+	//if answer is yes the create account method is called 
+	if (accountAnswer == 'Y' || accountAnswer == 'y')
+		createAccount(); 
+
+}
